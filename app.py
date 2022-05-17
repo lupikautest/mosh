a29f9cb4abd6d8200119d7f7047496e8610da2ce
lupikautest <lupikautest@gmail.com>
2022-05-17 17:08:18 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,269 @@
+# a = 2
+# print(a)
+
+# students_count = 1000
+# rating = 4.99
+# is_published = True
+# course_name = 'Python Programming'
+
+# print(len(course_name))
+# print(course_name[0:3])
+
+# course = "Python \nProgramming"
+# print(course)
+
+# first = 'Mosh'
+# last = 'Hamedani'
+# full = f'{first} {last}'
+# print(full)
+
+#course = 'python programming'
+# print(course.upper())
+# print(course.lower())
+# print(course.strip())
+# print(course.find('pro'))
+# print(course.replace('p', 'j'))
+# print('pro' in course)
+# print('swift' not in course)
+
+
+# print(round(2.5))
+# print(abs(-2.9))
+
+# x = input ('x: ')
+# y = int(x) + 1
+# print(f"x: {x}, y: {y}")
+
+# fruit = 'Apple'
+# print(fruit[0:-1])
+
+# print(17 % 5)
+
+# temperature = input("What is temperature?")
+# if int(temperature) > 30:
+#    print("It's warm")
+#    print("Drink water")
+# elif int(temperature) < 30:
+#    print("It's cold")
+#    print("Run Forest")
+# print("Done")
+
+# age = 22
+# if age >= 18:
+#    message = 'Eligible'
+# else:
+#    message = 'Not eligible'
+# print(message)
+
+# age = 12
+# message = 'Eligible' if age >= 18 else 'Not eligible'
+# print(message)
+
+# igh_income = False
+# good_credit = True
+# student = True
+
+# if not student
+#    print('Eligible')
+
+# age should be between 18 and 65
+
+# age = 22
+# if age >= 18 and age <65:
+# if 18 <= age <65:
+#    print("Eligible")
+
+# for number in range(3):
+#    print("Attempt", number + 1, (number +1) * ".")
+
+# for number in range(1, 10, 2):
+#    print("Attempt", number, number * ".")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#    print(x)'
+
+# x = 'Hello'
+# for i in range(1, 9):
+#    print(x, i * '.')
+
+# for x in range(5):
+#    for y in range(3):
+#        print(f"{x + 1}, {y}")
+
+# for x in 'Python':
+#    print(x)
+
+# for x in [1, 2, 3, 4, 5]:
+#    print(x)
+
+#number = 100
+#while number > 0:
+#    print(number)
+#    number -= 2
+
+#command = ''
+#while command.lower() != 'quit':
+#    command = input(">")
+#    print("ECHO", command)
+
+#while True:
+#    command = input('>')
+#    print('ECHO', command)
+#    if command.lower == 'quit':
+#        break
+
+#count = 0
+#for number in range(0, 20):
+#    if number % 2 == 0:
+#        count += 1
+#        print(number)
+#print(f'We have {count} even numbers')
+
+#x = int(input())
+#while x > 5:
+#    print(x)
+
+#number = 4
+#if number > 5:
+#    print(number * number)
+#else: print('number < than 5')
+
+#password = input('Enter password:')
+#if password == 'Sagitec@123':
+#    print('correct password')
+#else: print('invalid password')
+
+#def user_check(choice):
+#    if choice == 1:
+#        print("Admin")
+#    elif choice == 2:
+#        print("Editor")
+#    elif choice == 3:
+#        print("Guest")
+#    else:
+#        print("Wrong entry")
+#
+#user_check(int(input("Enter your choice")))
+
+#num1 = int(input('Enter first number '))
+#num2 = int(input('Enter second number '))
+
+#if num1 >= num2:
+#    if num1 == num2:
+#        print(num1, 'and', num2, 'are equal')
+#    else:
+#        print(num1, 'is greater than', num2)
+#else:
+#    print(num1, 'is smaller than', num2)
+
+#for i in range(10):
+#    print(i + 1)
+
+#num = 10
+#sum = 0
+#i = 1
+#while i <= num:
+#    sum = sum + i
+#    i = i + 1
+#print("Sum of first 10 number is:", sum)
+
+#for num in range(10):
+#    if num > 5:
+#        print('stop processing')
+#        break
+#    print(num)
+
+#for num in range(3, 9, 2):
+#    print(num)
+
+#for num in range(3, 8):
+#    if num == 5:
+#        continue
+#    else:
+#        print(num)
+
+#sum = 0
+#for x in range(10, 22, 2):
+#    sum = sum + x
+#print(sum)
+
+#numbers = [1, 2, 3, 4, 5]
+#numbers2 = [1, 2, 3, 4, 5]
+# iterate over each element in list num
+#for i in numbers:
+    # ** exponent operator
+#    square = i ** 2
+#    print("Square of:", i, "is:", square)
+
+#numbers = [10, 20, 30, 40, 50]
+#sum = 0
+#for i in numbers:
+#    sum = sum + i
+#list_size = len(numbers)
+#average = sum / list_size
+#print(average)
+
+#def addition(num1, num2):
+#    return num1 + num2
+#result = addition(10, 20)
+#print(result)
+
+# number_1 = 50
+# number_2 = 30
+#
+# if number_1 * number_2 >= 1000:
+#     print('product')
+# else:
+#     print(number_1 * number_2)
+
+# num = 543
+# if num%2==0:
+#     print("This number is even")
+# else:
+#     print("This is an odd number")
+
+# num=15
+# print("This number is even") if num%2==0 else print ("This number is odd")
+
+# num=-5
+# if num>=0:
+#     print("this is a positive number")
+# else:
+#     print("This is a negative number")
+
+
+# num1 = input(" Please enter number 1 : ")
+# num2 = input("Please enter number 2 : ")
+# num3 = input("Please enter number 3 : ")
+#
+# if num1 > num2 and num1 > num3:
+#     largest = num1
+# elif num2 > num1 and num2 > num3:
+#     largest = num2
+# else:
+#     largest = num3
+#
+# print("The largest number is = ")
+
+# new_string = "Germany26China47Australia88"
+#
+# emp_str = ""
+# for m in new_string:
+#     if m.isdigit():
+#         emp_str = emp_str + m
+# print(emp_str)
+
+# print(range(10))
+
+# x = 4
+# while x < 10:
+#     print(x)
+#     x=x+1
+
+# i = 10
+#
+# while i >= 0:
+#     print(i)
+#     i=i-1
+# print("DONE!!!")
